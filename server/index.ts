32e52bb0bf36b905dba245a2902f8e37a2fa7864
DevManDan1178 <l3r0d4niel@gmail.com>
2026-07-16 00:42:24 -0400

    + score streams in /api/score-stream

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import leaderboardHandler from "./api/leaderboard";
 import nameboardHandler from "./api/nameboard"
+import scoreStreamHandler from "./api/scoreStream"
 import "dotenv/config";
 
 const app = express();
@@ -23,6 +24,20 @@ app.all("/api/leaderboard", async (req, res) => {
 
 app.all("/api/nameboard", async (req, res) => {
     const response = await nameboardHandler(
+        new Request(`http://${req.headers.host}${req.url}`, {
+            method: req.method,
+            headers: req.headers as Record<string, string>,
+            body: req.method === "GET" ? undefined : JSON.stringify(req.body),
+        })
+    );
+
+    const data = await response.json();
+
+    res.status(response.status).json(data);
+});
+
+app.all("/api/score-stream", async (req, res) => {
+    const response = await scoreStreamHandler(
         new Request(`http://${req.headers.host}${req.url}`, {
             method: req.method,
             headers: req.headers as Record<string, string>,
