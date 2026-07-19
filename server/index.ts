60ca7e26e8199df26e0638bf84d9070de936b530
DevManDan1178 <l3r0d4niel@gmail.com>
2026-07-19 00:56:00 -0400

    + req with json

--- server/index.ts
+++ server/index.ts
@@ -6,33 +6,35 @@ import "dotenv/config";
 import { leaderboardApiURLPath } from "../shared/types/api/globalBoards/leaderboard";
 import { scoreStreamApiURLPath } from "../shared/types/api/globalBoards/scoreStreams";
 import { nameboardApiURLPath } from "../shared/types/api/globalBoards/nameboard";
+import { Request, Response } from "express";
 
 const app = express();
 
 app.use(express.json());
 
-app.all(`${leaderboardApiURLPath}`, async (req, res) => {
-    const response = await leaderboardHandler(
-        new Request(`http://${req.headers.host}${req.url}`, {
+function getReq(req : Request) : globalThis.Request {
+    return new Request(
+        `http://${req.headers.host}${req.url}`,
+        {
             method: req.method,
-            headers: req.headers as Record<string, string>,
-            body: JSON.stringify(req.body),
-        })
-    );
-
+            headers: {
+                "Content-Type": req.headers["content-type"] ?? "application/json",
+            },
+            body: JSON.stringify(req.body)
+        }
+    )
+}
+app.all(`${leaderboardApiURLPath}`, async (req, res) => {
+    const request = getReq(req);
+    const response = await leaderboardHandler(request);
     const data = await response.json();
 
     res.status(response.status).json(data);
 });
 
 app.all(`${nameboardApiURLPath}`, async (req, res) => {
-    const response = await nameboardHandler(
-        new Request(`http://${req.headers.host}${req.url}`, {
-            method: req.method,
-            headers: req.headers as Record<string, string>,
-            body: JSON.stringify(req.body),
-        })
-    );
+    const request = getReq(req);
+    const response = await nameboardHandler(request);
 
     const data = await response.json();
 
@@ -40,13 +42,8 @@ app.all(`${nameboardApiURLPath}`, async (req, res) => {
 });
 
 app.all(`${scoreStreamApiURLPath}`, async (req, res) => {
-    const response = await scoreStreamHandler(
-        new Request(`http://${req.headers.host}${req.url}`, {
-            method: req.method,
-            headers: req.headers as Record<string, string>,
-            body: JSON.stringify(req.body),
-        })
-    );
+    const request = getReq(req);
+    const response = await scoreStreamHandler(request);
 
     const data = await response.json();
 
