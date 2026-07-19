b2b8b35ea92b2fc107600c5f9b0c7095573ba575
DevManDan1178 <l3r0d4niel@gmail.com>
2026-07-19 17:39:01 -0400

    + high score leaderboard

--- client/src/pages/games/echoArena/EchoArenaGamePage.tsx
+++ client/src/pages/games/echoArena/EchoArenaGamePage.tsx
@@ -1,9 +1,10 @@
-import type { ReactElement } from "react";
+import { type ReactElement, useState } from "react";
 import { styles } from "../../../style";
 import SEO from "../../../components/misc/SEO";
 import GodotGame from "../../../components/games/GodotGame";
 import { echoArena } from "../../../assets";
 import type { GameEventLinkers } from "../../../../types/exhibits/games";
+import SubmittableLeaderboard from "../../../components/globalLists/SubmittableLeaderboard";
 
 const FILE_PATH = "/games/EchoArena/index.html";
 
@@ -39,16 +40,36 @@ const descriptionElement: ReactElement = (
 );
 
 export default function EchoArenaGamePage() {
-
+  const [leaderboard, attemptSumbitScore] = SubmittableLeaderboard({
+    category:"Echo Arena",
+    title: "Highest scores",
+    count: 20,
+    theme: {
+      font: "font-pixeloid"
+    },
+    bestScoreTitle: "BEST SCORE",
+    placeholderName: "Name",
+    submitButtonText: "SUBMIT"
+  })
 
   const gameEventLinkers : GameEventLinkers = [
     {
       gameEventName: "onHighscoreChanged",
-      handler: (score : number) => {
-        console.log("Echo Arena highscore: ", score)
-      }
+      handler(score : number) {
+        console.log("new godot highscore", score);
+        attemptSumbitScore(score);
+      },
     }
   ];
+
+  const [leaderboardToggled, setleaderboardToggled] = useState(false)
+
+  const [echoArenaGame, handleFullscreen] = GodotGame({
+    gamePath:FILE_PATH,
+    gameEventLinkers:gameEventLinkers,
+    className:"w-[calc(50%+125px)]"
+  })
+
   return (
     <>
       <SEO
@@ -63,13 +84,34 @@ export default function EchoArenaGamePage() {
         </div>
 
         <div className="flex-1 flex flex-col justify-center items-center">
-          <GodotGame
-            className="w-[calc(50%+125px)]"
-            gamePath={FILE_PATH}
-            gameEventLinkers={gameEventLinkers}
-          />
+        {echoArenaGame}
+        </div>
+        <div className="w-full flex justify-center items-start gap-20 pt-5 pb-10">
+          <div className="w-[calc(15%+50px)] flex justify-center">
+            <button
+              onClick={handleFullscreen}
+              className="mt-4 px-6 py-2 bg-white text-black font-pixeloid text-sm rounded hover:bg-zinc-300 transition"
+            >
+              FULLSCREEN
+            </button>
+          </div>
+
+          <div className="w-[calc(15%+50px)] flex justify-center">
+            <button
+              className="mt-4 px-6 py-2 bg-white text-black font-pixeloid text-sm rounded hover:bg-zinc-300 transition"
+              onClick={() => setleaderboardToggled(!leaderboardToggled)}
+            >
+              LEADERBOARD
+            </button>
+          </div>
         </div>
 
+        {leaderboardToggled && (
+          <div className="w-full flex justify-center pb-10 font-pixeloid">
+            {leaderboard}
+          </div>
+        )}
+                
         <div className="shrink-0 w-full flex justify-center text-sm text-zinc-400 py-5">
           <div className="w-full max-w-[80%] text-center">
             {descriptionElement}
