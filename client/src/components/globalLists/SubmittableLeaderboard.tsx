ca9b6b54fac7578f6e70eba7127e8bf20c5a273a
DevManDan1178 <l3r0d4niel@gmail.com>
2026-07-21 18:45:40 -0400

    ~refactor

--- client/src/components/globalLists/SubmittableLeaderboard.tsx
+++ client/src/components/globalLists/SubmittableLeaderboard.tsx
@@ -4,7 +4,7 @@ import { styles } from "../../style"
 import { type LeaderboardProps, Leaderboard } from "./Leaderboard"
 import { postQueryNetworkErrorCode, postQueryRefusedErrorCode, queryErrorCode } from "../../constants/components/globalLists"
 import { getThemeStyles } from "../../style"
-import { getOnKeyDownInputEventDuplicator } from "../../constants/components/globalBoards/input"
+import { getOnKeyDownInputEventDuplicator } from "../../constants/components/input/input"
 
 export type SubmittableLeaderboardProps = LeaderboardProps & SubmittableGlobalBoardPropsBase &  {
     scoreFormatFunction? : (score : number | undefined) => string
@@ -145,6 +145,7 @@ export default function({
               disabled={submitBlocked()}
               onKeyDown={getOnKeyDownInputEventDuplicator(setSubmitName, (candidateName : string) => candidateName.length < maxNameLength)}            
               placeholder={placeholderName}
+              onChange={() => {}}
               maxLength={20}
               className={`${getThemeStyles(theme)} w-full mb-5 mt-5 px-2 py-2 rounded-lg border-2 border-white/10 bg-white/10 text-white placeholder-white/40 text-center focus:outline-none focus:border-secondary`}
             />
