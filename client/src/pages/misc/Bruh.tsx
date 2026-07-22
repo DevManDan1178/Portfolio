ba8f993035694135cfed228c18eb6fbedb5c4f20
DevManDan1178 <l3r0d4niel@gmail.com>
2026-07-22 01:15:12 -0400

    + useref to prevent black screen after state change

--- client/src/pages/misc/Bruh.tsx
+++ client/src/pages/misc/Bruh.tsx
@@ -1,14 +1,13 @@
-import { useState } from "react";
-import GetYoutubeEmbed from "../../components/effects/YoutubeEmbed";
+import { useRef, useState } from "react";
+import GetYoutubeEmbedComponent from "../../components/effects/YoutubeEmbed";
 import SEO from "../../components/misc/SEO";
 
 const VIDEO_ID = "NZzXzymUgEg";
 
 const Bruh = () => {
   const [showVideo, setShowVideo] = useState(false);
-
-  const VideoComponent = GetYoutubeEmbed(VIDEO_ID, true, 22);
-
+  const getVideoComponent = GetYoutubeEmbedComponent(VIDEO_ID, true, 22)
+  const video = useRef(getVideoComponent);
   return (<>
     <SEO title="SECRET PORTFOLIO REAL" description="There's nothing to see here. Really."/>
     <div
@@ -26,7 +25,7 @@ const Bruh = () => {
           Click me?
         </button>
       ) : (
-        <VideoComponent />
+        <video.current/>
       )}
     </div>
   </>);
