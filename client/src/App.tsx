bb0110986592ad4fd5b13d02e8830f57d0f6b519
DevManDan1178 <l3r0d4niel@gmail.com>
2026-07-22 01:22:31 -0400

    + 404  glungus

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PolygonTDGamePage from "./pages/games/polygonTD/PolygonTDGamePage";
 import EchoArenaGamePage from "./pages/games/echoArena/EchoArenaGamePage";
 import MainPage from "./pages/MainPage";
-import Bruh from "./pages/misc/Bruh";
 import Sidestep2GamePage from "./pages/games/sidestep2/Sidestep2GamePage";
 import OriginalSoundtracksPage from "./pages/exhibits/OriginalSoundtracks";
 import GamesPage from "./pages/exhibits/Games";
 import ProjectsPage from "./pages/exhibits/Projects";
+import Glungus404 from "./pages/misc/Glungus404Page";
 
 
 function App() {
@@ -18,10 +18,10 @@ function App() {
             <Route path="/PolygonTD" element={<PolygonTDGamePage/>}/>  
             <Route path="/EchoArena" element={<EchoArenaGamePage/>}/>
             <Route path="/Sidestep2" element={<Sidestep2GamePage/>}/>
-            <Route path="/SecretRealPortfolio" element={<Bruh/>}/>
             <Route path="/OST" element={<OriginalSoundtracksPage/>}/>
             <Route path="/Play" element={<GamesPage/>} />
             <Route path="/Projects"  element= {<ProjectsPage/>}/>
+            <Route path="/404-not-found" element={<Glungus404/>}/>
           </Routes>
         </BrowserRouter>
     </div>    
