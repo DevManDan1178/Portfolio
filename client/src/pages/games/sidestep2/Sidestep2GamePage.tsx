2b4056f4c5aaa4dd8253e69c554f447ad4156600
DevManDan1178 <l3r0d4niel@gmail.com>
2026-08-12 20:33:06 -0400

    + full height for bg

--- client/src/pages/games/sidestep2/Sidestep2GamePage.tsx
+++ client/src/pages/games/sidestep2/Sidestep2GamePage.tsx
@@ -124,7 +124,7 @@ export default function Sidestep2GamePage() {
         url={seoInfo.url}
       />
 
-      <div className="w-full h-screen flex flex-col items-center p-10 bg-zinc-950 text-white">
+      <div className="w-full h-full flex flex-col items-center p-10 bg-zinc-950 text-white">
         <div className="text-3xl font-semibold">
           {titleElement}
         </div>
